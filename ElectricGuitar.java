93e9e90914f52e8cff5d47c75c154b62ad45b79b
YanXuVanderbilt <44687577+YanXuVanderbilt@users.noreply.github.com>
2018-12-10 12:42:28 -0600

    Add files via upload

---
+++ ElectricGuitar.java
@@ -0,0 +1,26 @@
+class ElectricGuitar {
+
+  String brand;
+  int numOfPickups;
+  boolean rockStarUsesIt;
+  
+  String Getbrand() {
+    return brand;
+    }
+    
+  void setBrand(String aBrand) {
+    brand = aBrand;
+    }
+    
+  int getNumOfPickups(int num) {
+    numOfPickups = num;
+    }
+    
+  boolean getRockStarUsesIt() {
+    return rockStarUsesIt;
+    }
+    
+  void setRockStarUsesIt(boolean yesOrNo) {
+    rockStarUsesIt = yesOrNo;
+    }
+}
---
+++ ExTestDrive.java
@@ -0,0 +1,29 @@
+class MyEx extends Exception {}
+
+public class ExTestDrive {
+
+  static void doRisky(String t) throws MyEx {
+    System.out.print("h");
+    if ("yes".equals(t)) {
+      throw new MyEx();
+     } 
+	}
+  
+  public static void main(String[] args) {
+    String test = args[0];
+    System.out.print("t");
+    
+    try 
+      {
+        doRisky(test);
+        System.out.print("a");
+      } catch (MyEx e) {
+      System.out.print("r");
+      System.out.print("o");
+    } finally {
+      System.out.print("w");
+      System.out.print("s");
+      }
+    }
+}
+      
